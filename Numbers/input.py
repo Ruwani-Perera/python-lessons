bbcba44ad0dc221598c738adea55160cb9539cdb
Ruwani-Perera <gayathrikadtr@gmail.com>
2021-09-26 03:20:12 +0530

    Number_functions

---
+++ Numbers/input.py
@@ -0,0 +1,14 @@
+# input() - get input from the user
+
+x = input("x:")
+# to perfrom following function, x is needed to convert into a int, initially x is a string
+y = int(x)+1
+print(y)
+
+# int(x), float(x), bool(x), str(x)
+
+# to find the type of x
+print(type(x))
+print(type(y))
+
+print(f"x:{x}, z:{int(y)+5}")
