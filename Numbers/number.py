ad18fc498577d7d55d041cebdeff5d5235e6fddf
Ruwani-Perera <gayathrikadtr@gmail.com>
2021-09-26 03:01:17 +0530

    +, -, *, /, //, %, **

---
+++ Numbers/number.py
@@ -0,0 +1,14 @@
+x = 1  # int
+y = 1.1  # float
+a = 1+2j  # complex no. >> a+bi, i = imagenary no
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10 % 3)
+print(10**3)  # 10^3
+
+print(round(2.9))
+print(abs(-2.6))  # absolute value, in here abs value is 2.6
