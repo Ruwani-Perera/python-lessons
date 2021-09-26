fc0e128c0cc592db2c4a072cafce75a08c867707
Ruwani-Perera <gayathrikadtr@gmail.com>
2021-09-26 13:49:36 +0530

    More for loop examples added

--- For loop/for_loop.py
+++ For loop/for_loop.py
@@ -1,2 +1,14 @@
 for number in range(3):
     print("Attempt", number)
+
+
+for number in range(3):
+    print("Attempt", number+1, (number+1)*".")
+
+# series statrt from 1 and will end before 4
+for number in range(1, 4):
+    print("Attempt", number, number*"#")
+
+# series start from 1 and will end before 10, final no represent the steps
+for number in range(1, 10, 2):
+    print("Attempt", number, number*"@")
