2fab33d06fd55c730225f4d0877e0967354bfeb8
Ruwani-Perera <gayathrikadtr@gmail.com>
2021-09-26 14:14:25 +0530

    For else

---
+++ For loop/for_else_loop.py
@@ -0,0 +1,10 @@
+eligibility = False
+for number in range(3):
+    print("Attempt", number+1)
+    if eligibility:
+        print("succesful")
+        # using this break, whole if (line 4,5,6) will exempt from execution.
+        break
+    # if the break in line here, this means for loop will break
+else:
+    print("Attempt", number+1, "times and failed")
